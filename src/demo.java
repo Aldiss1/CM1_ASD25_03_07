77b1849d16043265108525db5f8dbe5565aa05ca
Aldiss1 <aldisuryasaputra693@gmail.com>
2025-04-15 09:35:50 +0700

    add demo

---
+++ src/demo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("================== Toko Manasuka ==================");
+            System.out.println("BANK MENU:");
+            System.out.println("1. Data Barang");
+            System.out.println("2. Data Transaksi");
+            System.out.println("3. Pencarian Data");
+            System.out.println("4. Pengurutan Data");
+            System.out.println("5. Tampilkan Kode Terbalik");
+            System.out.println("6. Exit");
+            System.out.print("Choose Menu(1-6): ");
+
+        }
+    }
+}
